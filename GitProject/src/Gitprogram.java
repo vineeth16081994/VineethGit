d2004e7f588a4b886bb54856767a33d189f3ac6a
vineeth16081994 <vineeth.mrcool3@gmail.com>
2020-01-29 01:52:09 +0530

    hu

--- GitProject/src/Gitprogram.java
+++ GitProject/src/Gitprogram.java
@@ -7,14 +7,13 @@ public class Gitprogram {
 
 
 
-<<<<<<< HEAD
-System.out.println("hello12345");
-	System.out.println("hello");
-	System.out.println("hello");
-=======
+
 System.out.println("hello123456");
-	System.out.println("hello");	
->>>>>>> branch 'master' of https://github.com/vineeth16081994/VineethGit.git
+	System.out.println("hello");
+	System.out.println("hello");
+
+	
+
 
 	}
 
